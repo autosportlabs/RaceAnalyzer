0e531ef77fe4e61aa9878df5a7a3bf5c0fb41376
Brent Picasso <brent@autosportlabs.com>
2013-04-26 06:30:39 -0700

    disabled verbose logging

--- src/raceAnalyzer.cpp
+++ src/raceAnalyzer.cpp
@@ -136,7 +136,7 @@ MainFrame::MainFrame(const wxString &title, const wxPoint &pos, const wxSize
 	InitComms();
 
 	InitDatalogPlayer();
-	EnableVerbose(true);
+	EnableVerbose(false);
 
 	try{
 	 LoadInitialConfig();
